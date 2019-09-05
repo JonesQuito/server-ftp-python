7a26a1b48b7d1c2904d568de9cc6599ef0889998
JonesQuito <jonesdhy@hotmail.com>
2019-09-05 04:01:02 -0300

    nova organizacao de diretorios

--- serializarProject/lista_01/server.py
+++ serializarProject/lista_01/server.py
@@ -1,23 +1,5 @@
-class Funcionario(object):
-    def __init__(self, nome, cargo, salario):
-        self.nome = nome
-        self.cargo = cargo
-        self.salario = salario
-    
-    def calcula_reajuste(self):
-        if self.cargo.lower() == 'operador':
-            return self.salario * (1.2)
-        elif self.cargo.lower() == 'programador':
-            return self.salario * (1.18)
-        else:
-            return self.salario
-
-
-    def toString(self):
-        retorno = self.nome + ' recebe ' + str(self.calcula_reajuste())
-        return retorno
-
 import socket
+from models.funcionario import Funcionario
 
 # Declara as constantes de conexao
 HOST = ''
